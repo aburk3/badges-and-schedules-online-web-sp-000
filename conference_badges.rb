cf32a6f686fbb77d2920bbecc87688e45b9e3fc4
Austin  Burke <aburk3@gmail.com>
2018-09-29 20:18:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+end
+
+
+def assign_rooms(attendees)
+    attendees.each_with_index "Hello #{name}! You'll be assigned to room #{room}!"
+  end 
+end
