354fc83db9f275ab491ff5359bb53c43719af4ad
Austin  Burke <aburk3@gmail.com>
2018-09-29 20:44:42 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,9 +10,9 @@ end
 
 def assign_rooms(attendees)
   my_array = []
-    attendees.each_with_index { |name, index|
+    attendees.each_with_index do |name, index|
       spot_in_line = index + 1
       my_array << "Hello, #{name}! You'll be assigned to room #{spot_in_line}!"
-    }
+  end
   return my_array
 end
